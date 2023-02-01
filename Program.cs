406f1725b7353a56088e53693dc3fea5b595a7b4
Винников Вадим Эдуардович <122885964+rozhevadimev@users.noreply.github.com>
2023-02-01 14:49:47 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,23 +1,12 @@
-﻿//Задание 8.
-Console.WriteLine("Please, Enter number1");
-int number1 = int.Parse(Console.ReadLine());
+//"Задание 8."
 
-Console.WriteLine("Please,Enter number2");
-int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Please, Enter number");
+int number = int.Parse(Console.ReadLine ()!);
 
-Console.WriteLine("Please,Enter number3");
-int number3 = int.Parse(Console.ReadLine());
-int max = number1;
+int count = 2;
 
-if (number2 > max)
+while (count <= number)
 {
-    max = number2;
-  
-}
-if (number3 > max)
-
-{max = number3;
-
-}
-
-Console.WriteLine($"the largest number out of three {max}");
+Console.WriteLine($"{count}"); 
+count += 2;
+ }
